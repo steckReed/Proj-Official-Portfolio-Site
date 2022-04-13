007defafb4b9f8b979a568ce6ed19342c2a747a5
steckreed <steckreed@gmail.com>
2022-04-12 21:32:35 -0400

    Added better functionality for intro message text Added Proper sass files

---
+++ View/Public/JS/intro-message-title-text.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  var introMessageTitleTextA = $('.intro-message-title-text-a'); // Get '.intro-message-title-text-a' Elements
+
+  // Item Grid Rows (Set/Unset)
+    $('#intro-message-title-text-container').hover(
+      function() {
+        introMessageTitleTextA.css('grid-row', 'unset');           // Unset grid rows of items
+        introMessageTitleTextA.removeClass('slide-in-top');        // Remove Animation
+      },
+      function() { introMessageTitleTextA.css('grid-row', '1'); }, // Set grid rows of items
+    );
+  // Item Grid Rows
+
+  // Display Last Hovered Item
+    introMessageTitleTextA.hover(
+      function() { $( this ).css('z-index', '3'); },               // Set item index to top
+      function() {
+        introMessageTitleTextA.css('z-index', 'unset');            // Unset all item indexs
+        $( this ).css('z-index', '2');                             // Set last item hovered to top
+      }
+    );
+  // Display Last Hovered Item
+});
