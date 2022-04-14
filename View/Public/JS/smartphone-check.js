e5ca2ce173056faf15d150bb4689f85d5ce5c95b
steckreed <steckreed@gmail.com>
2022-04-13 23:33:55 -0400

    Added Sidenav  & Mobile js check

---
+++ View/Public/JS/smartphone-check.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {      
+  var isMobile = false;
+  if( $('#smartphone-check').css('display')=='none') { isMobile = true; }
+
+  // if (isMobile == true) {
+    // // Execute on Mobile
+  // }
+  console.log(isMobile);
+});
