9a7a1e9617d3d5ed3ab2c3d6d33b0d800703f3db
steckreed <steckreed@gmail.com>
2022-04-15 11:01:26 -0400

    Intro message fix for mobile (Added Click)

--- View/Public/JS/intro-message-title-text.js
+++ View/Public/JS/intro-message-title-text.js
@@ -1,22 +1,40 @@
 $( document ).ready(function() {
-  var introMessageTitleTextA = $('.intro-message-title-text-a'); // Get '.intro-message-title-text-a' Elements
+  var introMessageTitleTextContainer = $('#intro-message-title-text-container');
+  var introMessageTitleTextA         = $('.intro-message-title-text-a');
+  var introMessageTitleMobileClick   = $('#intro-message-title-mobile-click');
+
+  // Mobile Click
+    introMessageTitleMobileClick.click(
+      function() {
+        introMessageTitleTextContainer.addClass('intro-message-title-text-container-hover');
+        introMessageTitleTextA.css('grid-row', 'unset');
+        introMessageTitleTextA.removeClass('slide-in-top-title-text');
+        introMessageTitleMobileClick.css('display', 'none');
+      }
+    );
+  // Mobile Click
 
   // Item Grid Rows (Set/Unset)
-    $('#intro-message-title-text-container').hover(
+    introMessageTitleTextContainer.hover(
       function() {
-        introMessageTitleTextA.css('grid-row', 'unset');           // Unset grid rows of items
-        introMessageTitleTextA.removeClass('slide-in-top-title-text');        // Remove Animation
+        introMessageTitleTextContainer.addClass('intro-message-title-text-container-hover');
+        introMessageTitleTextA.css('grid-row', 'unset');
+        introMessageTitleTextA.removeClass('slide-in-top-title-text');
       },
-      function() { introMessageTitleTextA.css('grid-row', '1'); }, // Set grid rows of items
+      function() { 
+        introMessageTitleTextContainer.removeClass('intro-message-title-text-container-hover');
+        introMessageTitleTextA.css('grid-row', '1'); 
+        introMessageTitleMobileClick.css('display', 'block');
+      }
     );
   // Item Grid Rows
 
   // Display Last Hovered Item
     introMessageTitleTextA.hover(
-      function() { $( this ).css('z-index', '3'); },               // Set item index to top
+      function() { $( this ).css('z-index', '3'); },
       function() {
-        introMessageTitleTextA.css('z-index', 'unset');            // Unset all item indexs
-        $( this ).css('z-index', '2');                             // Set last item hovered to top
+        introMessageTitleTextA.css('z-index', 'unset');
+        $( this ).css('z-index', '2');
       }
     );
   // Display Last Hovered Item
