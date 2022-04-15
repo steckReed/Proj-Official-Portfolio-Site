0a829df8e49d79d371a9f02c99ae1050173dd3e8
steckreed <steckreed@gmail.com>
2022-04-15 11:55:17 -0400

    Fixed section title row layout & Animation Added Animation js file

---
+++ View/Public/JS/animation.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+  setTimeout( function() { $('#project-showcase-title-container').addClass('slide-to-top'); }, 1350 );
+  setTimeout( function() { $('#front-end-dev-title-container').addClass('slide-to-top'); }, 1350 );
+  setTimeout( function() { $('#back-end-dev-title-container').addClass('slide-to-top'); }, 1350 );
+  setTimeout( function() { $('#software-engineer-title-container').addClass('slide-to-top'); }, 1350 );
+  setTimeout( function() { $('#tech-stack-title-container').addClass('slide-to-top'); }, 1350 );
+});
