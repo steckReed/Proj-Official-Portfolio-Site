d584b2f9d7af4f0163eccc62230f4f9cc12eff40
steckreed <steckreed@gmail.com>
2022-04-15 11:12:49 -0400

    Intro Msg Better Mobile Support & Condensed JS

--- View/Public/JS/intro-message-title-text.js
+++ View/Public/JS/intro-message-title-text.js
@@ -3,39 +3,50 @@ $( document ).ready(function() {
   var introMessageTitleTextA         = $('.intro-message-title-text-a');
   var introMessageTitleMobileClick   = $('#intro-message-title-mobile-click');
 
-  // Mobile Click
+  // Functions
+    function closeIntroContainer(){
+      introMessageTitleTextContainer.removeClass('intro-message-title-text-container-hover');
+      introMessageTitleTextA.css('grid-row', '1');  
+      introMessageTitleMobileClick.css('display', 'block');
+    }
+    function expandIntroContainer(){
+      introMessageTitleTextContainer.addClass('intro-message-title-text-container-hover');
+      introMessageTitleTextA.css('grid-row', 'unset');
+      introMessageTitleTextA.removeClass('slide-in-top-title-text');
+    }
+  // Functions
+
+  // Event Listeners
+    // Mobile Click
     introMessageTitleMobileClick.click(
       function() {
-        introMessageTitleTextContainer.addClass('intro-message-title-text-container-hover');
-        introMessageTitleTextA.css('grid-row', 'unset');
-        introMessageTitleTextA.removeClass('slide-in-top-title-text');
+        expandIntroContainer();
         introMessageTitleMobileClick.css('display', 'none');
       }
     );
-  // Mobile Click
 
-  // Item Grid Rows (Set/Unset)
+    // Item Grid Rows (Set/Unset)
     introMessageTitleTextContainer.hover(
-      function() {
-        introMessageTitleTextContainer.addClass('intro-message-title-text-container-hover');
-        introMessageTitleTextA.css('grid-row', 'unset');
-        introMessageTitleTextA.removeClass('slide-in-top-title-text');
-      },
-      function() { 
-        introMessageTitleTextContainer.removeClass('intro-message-title-text-container-hover');
-        introMessageTitleTextA.css('grid-row', '1'); 
-        introMessageTitleMobileClick.css('display', 'block');
-      }
+      function() { expandIntroContainer(); },
+      function() { closeIntroContainer(); }
     );
-  // Item Grid Rows
 
-  // Display Last Hovered Item
-    introMessageTitleTextA.hover(
-      function() { $( this ).css('z-index', '3'); },
-      function() {
-        introMessageTitleTextA.css('z-index', 'unset');
-        $( this ).css('z-index', '2');
-      }
-    );
-  // Display Last Hovered Item
+    // Display Last Hovered Item
+      introMessageTitleTextA.hover(
+        function() { $( this ).css('z-index', '3'); },
+        function() {
+          introMessageTitleTextA.css('z-index', 'unset');
+          $( this ).css('z-index', '2');
+        }
+      );
+      introMessageTitleTextA.click(
+        function() { 
+          closeIntroContainer();
+
+          introMessageTitleTextA.css('z-index', 'unset');
+          $( this ).css('z-index', '2');
+        }
+      );
+    // Display Last Hovered Item
+  // Event Listeners
 });
