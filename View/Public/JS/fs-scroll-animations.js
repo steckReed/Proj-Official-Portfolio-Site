4cdda241c6ece88cb1500e6d0dff9ad59f9eccfc
steckreed <steckreed@gmail.com>
2022-04-26 23:05:18 -0400

    Minor animation js and style updates

--- View/Public/JS/fs-scroll-animations.js
+++ View/Public/JS/fs-scroll-animations.js
@@ -1,25 +1,26 @@
-
-    //constantly grabs the center of the screen to be used in checking whether an element is on screen
     var sa_CenterScreenCalc;
     function scrollAnimationActivate() { sa_CenterScreenCalc = $(window).scrollTop() + ($(window).height() / 2); }
 
     function scrollAnimationGetWindow(sa_TopEleId, sa_BottomEleId){
         try  { var sa_TopEleIndicator = $(sa_TopEleId).offset().top; }
         catch{ var sa_TopEleIndicator = 0; }
-        
         var sa_BottomEleIndicator = $(sa_BottomEleId).offset().top + $(sa_BottomEleId).height();
 
         return (sa_BottomEleIndicator >= sa_CenterScreenCalc) && (sa_TopEleIndicator <= sa_CenterScreenCalc)
     }
 
-    function scrollAnimation(sa_EleId, sa_TopEleId, sa_BottomEleId, sa_AnimationIn, sa_TimeDelay=300, sa_AnimationOut='') {
-      setTimeout(() => { 
+    // https://stackoverflow.com/questions/24596027/javascript-only-using-specific-parameter-in-function
+    // https://stackoverflow.com/questions/11796093/is-there-a-way-to-provide-named-parameters-in-a-function-call-in-javascript
+    function scrollAnimation(sa_EleId, sa_TopEleId, sa_AnimationIn, sa_TimeDelay=300, sa_AnimationOut='',  sa_BottomEleId='body', sa_RemoveAnimation=0) {
+      setTimeout(function(){ 
         if(scrollAnimationGetWindow(sa_TopEleId, sa_BottomEleId)) {
             $(sa_EleId).removeClass(sa_AnimationOut).addClass(sa_AnimationIn);
         }
       }, sa_TimeDelay);
 
-      if(!scrollAnimationGetWindow(sa_TopEleId, sa_BottomEleId)){
-          $(sa_EleId).removeClass(sa_AnimationIn).addClass(sa_AnimationOut);
+      if(sa_RemoveAnimation){ // Extra if to remove potentially un-needed calc
+        if(!scrollAnimationGetWindow(sa_TopEleId, sa_BottomEleId)) {
+            $(sa_EleId).removeClass(sa_AnimationIn).addClass(sa_AnimationOut);
+        }
       }
     }
