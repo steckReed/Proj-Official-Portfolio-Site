b7af28d767539d0b3b4d3627f88a089865c83c83
steckreed <steckreed@gmail.com>
2022-06-06 20:56:58 -0400

    Attempt to add Project front end toggle buttons

--- View/Public/JS/project-showcase-hover.js
+++ View/Public/JS/project-showcase-hover.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
   var projectShowcaseItemContainer     = $('.project-showcase-item-container');
   var projectShowcaseItem              = $('.project-showcase-item');
+  var projectShowcaseItemReferenceImg  = $('.project-showcase-item-reference-img');
   var projectShowcaseItemDescContainer = $('.project-showcase-item-desc-container');
   var projectShowcaseItemStackRibbon   = $('.project-showcase-item-stack-ribbon');
   
@@ -12,6 +13,7 @@ $( document ).ready(function() {
           var thisElement = $( this );
           var thisProjectShowcaseItem = thisElement.children('iframe');
           projectShowcaseItem.addClass('project-showcase-item-mini');
+          projectShowcaseItemReferenceImg.addClass('project-showcase-item-mini');
           thisProjectShowcaseItem.addClass('project-showcase-item-hover');
         // iFrame Expand
         
@@ -37,10 +39,17 @@ $( document ).ready(function() {
         projectShowcaseItemDescContainer.removeClass('slide-in-right-showcase');
         projectShowcaseItemDescContainer.css('width', '0px');
         projectShowcaseItem.removeClass('project-showcase-item-mini project-showcase-item-hover');
+        projectShowcaseItemReferenceImg.removeClass('project-showcase-item-mini project-showcase-item-hover');
         projectShowcaseItemContainer.css('z-index', 'unset');
         projectShowcaseItemStackRibbon.removeClass('slide-in-top-stack-ribbon');
         projectShowcaseItemDescContainer.css('width', '0px');
       }
     );
   // Event Listeners
+
+  $('.project-showcase-recreations-toggle').click(function () {
+    var thisElement = $( this );
+    thisElement.closest('.project-showcase-item-container').children('.project-showcase-item').toggleClass('dis-none');
+    thisElement.closest('.project-showcase-item-container').children('.project-showcase-item-reference-img').toggleClass('dis-none');
+  });
 });
