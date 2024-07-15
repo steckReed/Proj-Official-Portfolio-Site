96e80d1c5a666a4e07fe45a3ec6a779d5bc55532
steckreed <crs209@zips.uakron.edu>
2024-07-15 09:40:44 -0400

    deleted project

--- View/Public/JS/smartphone-check.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {      
-  var isMobile = false;
-  if( $('#smartphone-check').css('display')=='none') { isMobile = true; }
-
-  // if (isMobile == true) {
-    // // Execute on Mobile
-  // }
-  // console.log(isMobile);
-});
