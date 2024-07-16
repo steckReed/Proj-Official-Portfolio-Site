7694f34cf08d97b1acb23746d4d151b1b6a99f0f
steckreed <crs209@zips.uakron.edu>
2024-07-16 16:20:15 -0400

    moved components dir outside of app and into src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { Box } from '@mui/material';
-import HeroSection from './components/modules/Hero/HeroSection/HeroSection';
-import ProjectsCarousel from './components/modules/ProjectsCarousel/ProjectsCarousel';
-import NavBar from './components/modules/NavBar/NavBar';
+import HeroSection from '../components/modules/Hero/HeroSection/HeroSection';
+import ProjectsCarousel from '../components/modules/ProjectsCarousel/ProjectsCarousel';
+import NavBar from '../components/modules/NavBar/NavBar';
 
 export default function Home() {
   return (
     <div style={{ minHeight: '100vh'}} >
 
       <NavBar />
-      
+
       <Box sx={{ display: 'flex', minHeight:'100vh', flexWrap:'wrap', alignContent:'center', gap:'5.5vh'}}>
 
 
