2de3954312e751a4e15fdb9e3b5be4520c8775c5
steckreed <crs209@zips.uakron.edu>
2024-10-10 20:44:08 -0400

    finalized ChipCarousel for better smooth scroll animations & calcs

--- src/components/elements/ChipCarousel/ChipCarousel.tsx
+++ src/components/elements/ChipCarousel/ChipCarousel.tsx
@@ -6,11 +6,36 @@ interface ChipCarouselProps {
   directions  : Array<'left' | 'right'>;
   speeds      : Array<number>;
   colors      ?: Array<string>;
+  itemGap     ?: Array<number>;
 }
 
-const ChipCarousel: React.FC<ChipCarouselProps> = ({ rows, directions, speeds, colors }) => {
+const ChipCarousel: React.FC<ChipCarouselProps> = ({ 
+  rows, 
+  directions, 
+  speeds, 
+  colors,
+  itemGap
+}) => {
+  const [screenWidth, setScreenWidth] = useState(0);
   const [hovered, setHovered] = useState(false);
   const [scrollSpeedMultiplier, setScrollSpeedMultiplier] = useState(1);
+
+  // Set screenWidth
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+
+    // Function to update screen width on window resize
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
 
   // On scroll speed 
   useEffect(() => {
@@ -43,13 +68,15 @@ const ChipCarousel: React.FC<ChipCarouselProps> = ({ rows, directions, speeds, c
     >
       {rows.map((skills, rowIndex) => (
         <ChipRow
-          key={rowIndex}
-          skills={skills}
-          direction={directions[rowIndex]}
-          baseSpeed={speeds[rowIndex]}
-          hovered={hovered}
-          scrollSpeedMultiplier={scrollSpeedMultiplier}
-          color={colors?.[rowIndex] || '#2d3e50'}
+          screenWidth = {screenWidth}
+          key         = {rowIndex}
+          skills      = {skills}
+          direction   = {directions[rowIndex]}
+          baseSpeed   = {speeds[rowIndex]}
+          hovered     = {hovered}
+          color       = {colors?.[rowIndex] || '#2d3e50'}
+          itemGap     = {itemGap?.[rowIndex] || 10}
+          scrollSpeedMultiplier = {scrollSpeedMultiplier}
         />
       ))}
     </div>
@@ -59,36 +86,39 @@ const ChipCarousel: React.FC<ChipCarouselProps> = ({ rows, directions, speeds, c
 
 
 interface ChipRowProps {
-  skills    : string[];
-  direction : 'left' | 'right';
-  baseSpeed : number;
-  hovered   : boolean;
+  screenWidth : number;
+  skills      : string[];
+  direction   : 'left' | 'right';
+  baseSpeed   : number;
+  hovered     : boolean;
+  color       : string;
+  itemGap     : number;
   scrollSpeedMultiplier: number;
-  color     : string;
 }
 
 const ChipRow: React.FC<ChipRowProps> = ({
+  screenWidth,
   skills,
   direction,
   baseSpeed,
   hovered,
+  color,
+  itemGap,
   scrollSpeedMultiplier,
-  color
 }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const xTranslation = useMotionValue(0);
-  const [rowWidth, setRowWidth] = useState(0);
+  const [rowWidth, setRowWidth]     = useState(screenWidth);
   const [numRepeats, setNumRepeats] = useState(1);
+  const rowRef          = useRef<HTMLDivElement>(null);
+  const xTranslation    = useMotionValue(-rowWidth);
+  const positionFactor  = (direction === 'left') ?(-1) :(1);
+  const finalSpeed      = hovered ? (baseSpeed / 2) : (baseSpeed * scrollSpeedMultiplier); // Based on hover/still/scroll
 
-
-  // Get row and screen width to determine how many times to repeat row to fill screen
+  // Get row width & determine how many times to repeat row len to fill screen
   useEffect(() => {
     const handleResize = () => {
-      if (ref.current && containerRef.current) {
-        const rowWidth = ref.current.offsetWidth;
-        const screenWidth = containerRef.current.offsetWidth;
-        setRowWidth(rowWidth);
+      if (rowRef.current) {
+        const rowWidth = rowRef.current.getBoundingClientRect().width;
+        setRowWidth(rowWidth)
         
         const repeats = Math.ceil(screenWidth / rowWidth) + 1;
         setNumRepeats(repeats);
@@ -99,28 +129,23 @@ const ChipRow: React.FC<ChipRowProps> = ({
     window.addEventListener('resize', handleResize);
 
     return () => window.removeEventListener('resize', handleResize);
-  }, [skills]);
-
-
-  // Adjust speed based on hover and scroll
-  const finalSpeed = hovered ?(baseSpeed / 2) :(baseSpeed * scrollSpeedMultiplier);
+  }, [screenWidth]);
 
   useEffect(() => {
     let animationFrameId: number;
-
-    const finalPosition = direction === 'left' ? (-rowWidth) : rowWidth;
+    const finalPosition = rowWidth * positionFactor - itemGap;
 
     const updatePosition = () => {
       const current = xTranslation.get();
-      const delta = (direction === 'left' ? -1 : 1) * finalSpeed * 0.1;
+      const delta = positionFactor * finalSpeed * 0.1;
       let nextPosition = current + delta;
 
       // If at edge, loop back
-      if (
-        (direction === 'left' && nextPosition <= finalPosition) || 
-        (direction === 'right' && nextPosition >= finalPosition)
-      ) {
-        nextPosition = direction === 'left' ? 0 : -rowWidth;
+      if (direction === 'left') {
+        nextPosition = nextPosition <= finalPosition ?(nextPosition % finalPosition) :(nextPosition);
+      } else {
+        nextPosition = nextPosition >= 0 ?(((nextPosition % rowWidth) + rowWidth) % rowWidth - rowWidth) :(nextPosition);
+
       }
 
       xTranslation.set(nextPosition);
@@ -132,11 +157,11 @@ const ChipRow: React.FC<ChipRowProps> = ({
 
     // Cleanup function to cancel the animation frame when the component unmounts
     return () => cancelAnimationFrame(animationFrameId);
-  }, [xTranslation, finalSpeed, rowWidth, direction]);
+  }, [xTranslation, finalSpeed, rowWidth, direction, skills.length, positionFactor, itemGap]);
 
   return (
-    <div ref={containerRef} style={{ overflow: 'hidden', width: '100%' }}>
-      <motion.div ref={ref} style={{ x: xTranslation, display: 'flex', gap: '10px' }}>
+    <div style={{ overflow: 'hidden', width: '100%' }}>
+      <motion.div ref={rowRef} style={{ x: xTranslation, display: 'flex', gap: `${itemGap}px`, width:'fit-content' }}>
         {Array.from({ length: numRepeats }).map((_, repeatIndex) => (
           skills.map((skill, skillIndex) => (
             <div
