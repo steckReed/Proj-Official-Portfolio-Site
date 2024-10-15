9b3c2da3ccfde5d87784510bf1e3f69fd2447952
steckreed <crs209@zips.uakron.edu>
2024-10-15 17:17:44 -0400

    ChipCarousel.tsx - temp rm onscroll speed  + getScrollSpeedMultiplier to change speed based on screen width  + changed baseSpeed var to speed obj to prevent calcs being ran within the row, but instead the speed calcs are done by the parent and passed down

--- src/components/elements/ChipCarousel/ChipCarousel.tsx
+++ src/components/elements/ChipCarousel/ChipCarousel.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import ChipCarouselRow from './ChipCarouselRow/ChipCarouselRow';
+import getScrollSpeedMultiplier from '@/functions/GetScrollSpeedMultiplier';
 
 interface ChipCarouselProps {
   rows        : Array<string[]>;
@@ -18,7 +19,7 @@ const ChipCarousel: React.FC<ChipCarouselProps> = ({
 }) => {
   const [screenWidth, setScreenWidth]                     = useState(0);
   const [hovered, setHovered]                             = useState(false);
-  const [scrollSpeedMultiplier, setScrollSpeedMultiplier] = useState(1);
+  // const [scrollSpeedMultiplier, setScrollSpeedMultiplier] = useState(1);
 
   // Set screenWidth
   useEffect(() => {
@@ -26,7 +27,10 @@ const ChipCarousel: React.FC<ChipCarouselProps> = ({
 
     // Function to update screen width on window resize
     const handleResize = () => {
-      setScreenWidth(window.innerWidth);
+      const windowWidth: number = window.innerWidth;
+
+      setScreenWidth(windowWidth);
+      getScrollSpeedMultiplier({ screenWidth: windowWidth })
     };
 
     // Add event listener for window resize
@@ -37,22 +41,33 @@ const ChipCarousel: React.FC<ChipCarouselProps> = ({
   }, []);
 
 
-  // On scroll speed 
-  useEffect(() => {
-    const handleScroll = () => { setScrollSpeedMultiplier(2.5); };
+  /* Temp Removal of on scroll speed change
+    - Bit too buggy, in need of better method for smoothing animation
+  */
+  // // On scroll speed 
+  // useEffect(() => {
+  //   const handleScroll = () => { setScrollSpeedMultiplier(2.5); };
 
-    const handleScrollEnd = () => {
-      setTimeout(() => { setScrollSpeedMultiplier(1); }, 300);
-    };
+  //   // const handleScrollEnd = () => {
+  //   //   setTimeout(() => { setScrollSpeedMultiplier(1); }, 300);
+  //   // };
 
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('scroll', handleScrollEnd);
+  //   const handleScrollEnd = () => {
+  //     // Gradually reset speed after scroll ends
+  //     setTimeout(() => {
+  //       const adjustedMultiplier = getScrollSpeedMultiplier({ screenWidth });
+  //       setScrollSpeedMultiplier(adjustedMultiplier);
+  //     }, 300);
+  //   };
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('scroll', handleScrollEnd);
-    };
-  }, []);
+  //   window.addEventListener('scroll', handleScroll);
+  //   window.addEventListener('scroll', handleScrollEnd);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //     window.removeEventListener('scroll', handleScrollEnd);
+  //   };
+  // }, []);
 
   return (
     <div
@@ -66,19 +81,27 @@ const ChipCarousel: React.FC<ChipCarouselProps> = ({
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      {rows.map((skills, rowIndex) => (
-        <ChipCarouselRow
-          screenWidth = {screenWidth}
-          key         = {rowIndex}
-          skills      = {skills}
-          direction   = {directions[rowIndex]}
-          baseSpeed   = {speeds[rowIndex]}
-          hovered     = {hovered}
-          color       = {colors?.[rowIndex] || '#2d3e50'}
-          itemGap     = {itemGap?.[rowIndex] || 10}
-          scrollSpeedMultiplier = {scrollSpeedMultiplier}
-        />
-      ))}
+      {rows.map((skills, rowIndex) => {
+
+        const baseSpeed = speeds[rowIndex] * getScrollSpeedMultiplier({ screenWidth });
+
+        return(
+          <ChipCarouselRow
+            key         = {rowIndex}
+            screenWidth = {screenWidth}
+            skills      = {skills}
+            direction   = {directions[rowIndex]}
+            speed   = {{
+              'hovered' : baseSpeed / 2,
+              'base'    : baseSpeed
+            }}
+            hovered     = {hovered}
+            color       = {colors?.[rowIndex] || '#2d3e50'}
+            itemGap     = {itemGap?.[rowIndex] || 10}
+            // scrollSpeedMultiplier = {scrollSpeedMultiplier}
+          />
+        )
+      })}
     </div>
   );
 };
